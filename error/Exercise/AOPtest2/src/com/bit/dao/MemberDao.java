2af64bfc1d2b8408b79ac24170f0b74b01848500
ldh85246 <go85246@hanmail.net>
2020-04-14 12:36:31 +0900

    에러

---
+++ error/Exercise/AOPtest2/src/com/bit/dao/MemberDao.java
@@ -0,0 +1,13 @@
+package com.bit.dao;
+
+public class MemberDao {
+	public int insertMember(String name) {
+		System.out.println(name + "의 정보를 등록하였습니다.");
+		return 1;
+	}
+	
+	public int updateMember(String name) {
+		System.out.println(name + "의 정보를 등록하였습니다.");
+		return 1;
+	}
+}
